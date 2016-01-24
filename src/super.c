235533969abee3ed7e228b7247ec15e7acaa72b8
Bradley Theilman <bradtheilman@gmail.com>
2016-01-23 22:22:56 -0800

    Began superblock file containing bit allocation

---
+++ src/super.c
@@ -0,0 +1,13 @@
+/* super.c  : Superblock management */
+#include "sfs_types.h"
+#include "sfs_const.h"
+
+sfs_bit_t alloc_bit(sfs_super_block *sp, int map, sfs_bit_t origin)
+{
+
+}
+
+void free_bit(sfs_super_block *sp, int map, sfs_bit_t bit_to_return)
+{
+	
+}
