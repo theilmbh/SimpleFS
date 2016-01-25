aa5d2d1aec5666dafdc6b68f74147a98e9384466
Bradley Theilman <bradtheilman@gmail.com>
2016-01-24 17:10:16 -0800

    Skeleton of routines for superblock management

--- src/super.c
+++ src/super.c
@@ -9,5 +9,15 @@ sfs_bit_t alloc_bit(sfs_super_block *sp, int map, sfs_bit_t origin)
 
 void free_bit(sfs_super_block *sp, int map, sfs_bit_t bit_to_return)
 {
+
+}
+
+sfs_super_block_t *get_super(sfs_dev_t dev)
+{
+
+}
+
+sfs_super_block_t *read_super(sfs_dev_t dev)
+{
 	
 }
